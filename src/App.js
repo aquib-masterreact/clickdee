988366dd1cca45e6c27493ef0c54a4cdd3b3d8cf
khanice <you@example.com>
2023-07-06 01:24:31 +0530

    final

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <>
-    <div className="flex flex-col flex-wrap sm:flex-row ">
+    <div className="md:flex flex-col flex-wrap sm:flex flex-wrap ">
         <div className="flex flex-wrap">
-          <div className="w-[50%] text-center bg-red-50 p-10 ">
+          <div className="md:w-[50%] text-center bg-red-50 p-10 sm:w-[100%] ">
             <h1 className='text-2xl m-3'>Why The Industry Chooses Clickdee</h1>
             <p className="">
               Let our diverse marketing team advertise your services! We support
@@ -15,14 +15,14 @@ function App() {
               Plumbing, Electricians, HVAC, Home
             </p>
           </div>
-          <div className="w-[25%] text-center p-10 group/item hover:bg-slate-100 hover:scale-110">
+          <div className="md:w-[25%] text-center p-10 group/item hover:bg-slate-100 hover:scale-110 sm:w-[100%]">
             <h1 className='text-2xl m-3'>Choose Your Local Targeting</h1>
             <p className="group/edit invisible  group-hover/item:visible">
               Our targeted and tracked calls are tailored to your business
               needs, audience, and geolocation.
             </p>
           </div>
-          <div className="w-[25%] text-center bg-red-50 p-10 group/item hover:bg-slate-100 hover:scale-110">
+          <div className="md:w-[25%] text-center bg-red-50 p-10 group/item hover:bg-slate-100 hover:scale-110 sm:w-[100%]">
             <h1 className='text-2xl m-3 '>Track Your Conversions</h1>
             <p className="group/edit invisible  group-hover/item:visible">
               We use the most progressive tracking and analytics technology to
@@ -31,21 +31,21 @@ function App() {
           </div>
         </div>
         <div className="flex justify-end flex-wrap">
-          <div className="w-[25%] text-center p-10 group/item hover:bg-slate-100 hover:scale-110">
+          <div className="md:w-[25%] text-center p-10 group/item hover:bg-slate-100 hover:scale-110 sm:w-[100%]">
             <h1 className='text-2xl m-3'>Custamized Campaign </h1>
             <p className="group/edit invisible  group-hover/item:visible">
               Audiences, budgets, and goals: you decide the criteria for your
               digital ad campaign, and we’ll do the rest.
             </p>
           </div>
-          <div className="w-[25%] text-center bg-red-50 p-10 group/item hover:bg-slate-100 hover:scale-110">
+          <div className="md:w-[25%] text-center bg-red-50 p-10 group/item hover:bg-slate-100 hover:scale-110 sm:w-[100%]">
             <h1 className='text-2xl m-3'>Quality Assurance </h1>
             <p className="group/edit invisible  group-hover/item:visible">
               We constantly monitor quality control! Our affiliate network of
               publishers are digital ad experts that have been vetted.
             </p>
           </div>
-          <div className="w-[25%] text-center p-10 group/item hover:bg-slate-100 hover:scale-110">
+          <div className="md:w-[25%] text-center p-10 group/item hover:bg-slate-100 hover:scale-110 sm:w-[100%]">
             <h1 className='text-2xl m-3 text-align-center'>Get Dedicated Support Team</h1>
             <p className="group/edit invisible  group-hover/item:visible">
               We constantly monitor quality control! Our affiliate network of
